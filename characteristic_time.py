a8360a56483beb07b2bfe80e93371acd3f8e9344
rssalessio <arusso2@bu.edu>
2024-11-22 16:07:24 -0500

    add working characteristic time

--- characteristic_time.py
+++ characteristic_time.py
@@ -4,7 +4,7 @@ import dccp
 import numpy.typing as npt
 from enum import Enum
 from mdp import MDP
-from reward_set import RewardSet, RewardSetCircle
+from reward_set import RewardSet, RewardSetCircle, RewardSetType, RewardSetRewardFree, RewardSetBox
 from typing import NamedTuple
 
 
@@ -14,12 +14,14 @@ class BoundResult(NamedTuple):
     w: npt.NDArray[np.float64]
 
 class CharacteristicTimeSolver(object):
+    """ Class to solve the characteristic time problem """
     dim_state: int
     dim_actions: int
     theta: cp.Variable
     KG: cp.Parameter
     e_i: cp.Parameter
     MD_problem: cp.Problem
+    rewards: RewardSet
 
     def __init__(self, dim_state: int, dim_actions: int):
         self.dim_state = dim_state
@@ -29,11 +31,69 @@ class CharacteristicTimeSolver(object):
         self.e_i = cp.Parameter(dim_state)
 
     def build_problem(self, rewards: RewardSet):
+        """Build problem to improve speed
+
+        Args:
+            rewards (RewardSet): Set of rewards considered
+        """
         Ks = self.e_i.T @ self.KG @ self.theta
         obj = cp.Maximize(cp.abs(Ks))
         self.MD_problem = cp.Problem(obj, rewards.get_constraints(self.theta))
+        self.rewards = rewards
 
     def solve(self,
+              gamma: float, 
+              mdp: MDP,
+              policy: npt.NDArray[np.long]) -> BoundResult:
+        """Solve the characteristic time optimization problem
+
+        Args:
+            gamma (float): discount facotr
+            mdp (MDP): MDP considered
+            policy (npt.NDArray[np.long]): deterministic policy of size S
+
+        Returns:
+            BoundResult: A tuple containing the value of the problem and the optimal solution
+        """
+        if self.rewards.set_type == RewardSetType.FINITE:
+            raise Exception('Finite set not implemented')
+        elif self.rewards.set_type == RewardSetType.REWARD_FREE:
+            return self._solve_rewardfree(gamma, mdp, policy)
+        else:
+            return self._solve_general(gamma, mdp, policy)
+        
+    def _solve_rewardfree(self,
+              gamma: float, 
+              mdp: MDP,
+              policy: npt.NDArray[np.long]) -> BoundResult:
+        G = mdp.build_stationary_matrix(policy, gamma=gamma)
+        K = mdp.build_K(policy)
+
+        A = np.zeros((mdp.dim_state, mdp.dim_state))
+        for i in range(mdp.dim_state):
+            for s in range(mdp.dim_state):
+                KG = K[s] @ G
+                Ai_s = KG[i]
+                pos_idxs = Ai_s >= 0
+                
+                A[s,i] = np.maximum(Ai_s[pos_idxs].sum(-1), Ai_s[~pos_idxs].sum(-1))
+        A = (A ** 2).max(-1)
+
+        omega = cp.Variable((self.dim_state, self.dim_actions), nonneg=True)
+
+        
+        constraints = [cp.sum(omega) == 1]
+        constraints.extend(
+            [cp.sum(omega[s]) == cp.sum(cp.multiply(mdp.P[:,:,s], omega)) for s in range(self.dim_state)])
+    
+        omega_pi = omega[np.arange(mdp.dim_state), policy]
+
+        obj = cp.Minimize(cp.max(cp.multiply(A, cp.inv_pos(omega_pi))))
+        T_problem = cp.Problem(obj, constraints)
+        res = T_problem.solve()
+        return BoundResult(res, omega.value)
+
+    def _solve_general(self,
               gamma: float, 
               mdp: MDP,
               policy: npt.NDArray[np.long]) -> BoundResult:
@@ -70,10 +130,17 @@ if __name__ == '__main__':
     mdp = MDP.generate_random_mdp(3, 2)
     policy = np.array([0, 1, 0], dtype=np.long)
     rewards = RewardSetCircle(mdp.dim_state, np.zeros(mdp.dim_state), radius=1, p=2)
-
+    rbox = RewardSetBox(mdp.dim_state, np.zeros(mdp.dim_state), np.ones(mdp.dim_state))
+    rfree = RewardSetRewardFree(mdp.dim_state)
     solver = CharacteristicTimeSolver(mdp.dim_state, mdp.dim_action)
     solver.build_problem(rewards)
 
+    print(solver.solve(0.9, mdp, policy))
+
+    solver.build_problem(rfree)
+    print(solver.solve(0.9, mdp, policy))
+
+    solver.build_problem(rbox)
     print(solver.solve(0.9, mdp, policy))
 
     
