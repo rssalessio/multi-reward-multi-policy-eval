8b484120b09106c364cb49fdca3fd96a6f7fd7c5
rssalessio <arusso2@bu.edu>
2024-11-22 15:51:27 -0500

    add finite; box and rf rewards

--- reward_set.py
+++ reward_set.py
@@ -11,6 +11,7 @@ class RewardSetType(object):
     BOX = 'Box'
     CIRCLE = 'Circle'
     POLYTOPE = 'Polytope'
+    REWARD_FREE = 'RewardFree'
     NONE =  'None'
 
 
@@ -61,7 +62,7 @@ class RewardSetCircle(RewardSet):
 
     def satisfy_constraints(self, reward: npt.NDArray[np.float64]) -> bool:
         norm = np.linalg.norm(reward - self.center, ord=self.p)
-        return np.any([reward < 0, norm > self.radius])
+        return np.all([reward >= 0, reward <= 1, norm <= self.radius])
 
     def get_constraints(self, var: cp.Variable) -> Sequence[cp.Constraint]:
         constraints = [var >= 0, cp.norm(var - self.center, self.p) <= self.radius, var <= 1]
@@ -81,9 +82,63 @@ class RewardSetPolytope(RewardSet):
         self.b = b
 
     def satisfy_constraints(self, reward: npt.NDArray[np.float64]) -> bool:
-        return np.any([reward < 0, self.A @ reward > self.b])
+        return np.all([reward >= 0, reward <= 1, self.A @ reward <= self.b])
 
     def get_constraints(self, var: cp.Variable) -> Sequence[cp.Constraint]:
         constraints = [var >= 0, self.A @ var <= self.b, var <= 1]
         return constraints
 
+
+class RewardSetBox(RewardSet):
+    """ Bounds the reward set as a<=r<=b elementwise """
+    a: npt.NDArray[np.float64]
+    b: npt.NDArray[np.float64]
+
+    def __init__(self, num_states: int, a: npt.NDArray[np.float64], b: npt.NDArray[np.float64]):
+        super().__init__(num_states)
+        self.set_type = RewardSetType.BOX
+        self.a = a
+        self.b = b
+
+    def satisfy_constraints(self, reward: npt.NDArray[np.float64]) -> bool:
+        return np.all([reward >= np.maximum(0, self.a), reward <= np.minimum(1, self.b)])
+
+    def get_constraints(self, var: cp.Variable) -> Sequence[cp.Constraint]:
+        constraints = [var >= np.maximum(0, self.a), var <= np.minimum(1, self.b)]
+        return constraints
+
+
+class RewardSetRewardFree(RewardSet):
+    """ Consider the entire set [0,1]^SA """
+
+    def __init__(self, num_states: int):
+        super().__init__(num_states)
+        self.set_type = RewardSetType.REWARD_FREE
+
+    def satisfy_constraints(self, reward: npt.NDArray[np.float64]) -> bool:
+        return np.all([reward >= 0, reward <= 1])
+
+    def get_constraints(self, var: cp.Variable) -> Sequence[cp.Constraint]:
+        constraints = [var >= 0, var <= 1]
+        return constraints
+
+class RewardSetFinite(RewardSet):
+    """ Consider a finite set of M rewards, each of size S """
+
+    def __init__(self, num_states: int, rewards: npt.NDArray[np.float64]):
+        super().__init__(num_states)
+        self.set_type = RewardSetType.FINITE
+        self.rewards = rewards
+        self.num_rewards = rewards.shape[0]
+
+    def satisfy_constraints(self, reward: npt.NDArray[np.float64]) -> bool:
+        if np.any([reward < 0, reward > 1]): return False
+
+        for i in range(self.num_rewards):
+            if np.isclose(np.linalg.norm(reward - self.rewards[i]), 0):
+                return True
+        return False
+
+    def get_constraints(self, var: cp.Variable) -> Sequence[cp.Constraint]:
+        constraints = [var >= 0, var <= 1]
+        return constraints
